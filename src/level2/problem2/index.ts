3a832906b2adaf24fe8a57657b3ea33e0abb9b34
H-Frederick <31770906+H-Frederick@users.noreply.github.com>
2024-05-15 21:14:11 +0800

    Solve level 2 problem 2

--- src/level2/problem2/index.ts
+++ src/level2/problem2/index.ts
@@ -1,8 +1,33 @@
 export type DowntimeLogs = [Date, Date][];
 
 export function merge(...args: DowntimeLogs[]): DowntimeLogs {
-  /**
-   * insert your code here
-   */
-  return [];
+    let combinedLogs: DowntimeLogs = [];
+
+    let argLogs = args.flat();
+
+    argLogs.sort((a, b) => { 
+        return a[0].getTime() - b[0].getTime(); 
+    });
+
+    let [currentStart, currentEnd] = argLogs[0];
+
+    argLogs.forEach((value, i) => {
+        if (i === 0) {
+            return;
+        }
+
+        const [start, end] = value;
+        if (start > currentEnd) {
+            combinedLogs.push([currentStart, currentEnd]);
+            [currentStart, currentEnd] = [start, end];
+        } else {
+            if (end > currentEnd) {
+                currentEnd = end;
+            }
+        }
+    });
+
+    combinedLogs.push([currentStart, currentEnd]);
+
+    return combinedLogs;
 }
