33329bb0f1093c2d01307994b1c252160359aaf0
H-Frederick <31770906+H-Frederick@users.noreply.github.com>
2024-05-15 20:59:44 +0800

    solve level 2 problem 1. Add mutex package

--- src/level2/problem1/index.ts
+++ src/level2/problem1/index.ts
@@ -1,11 +1,27 @@
+import { Mutex } from "async-mutex";
+
+const mutex = new Mutex();
+
 export class ExecutionCache<TInputs extends Array<unknown>, TOutput> {
-  constructor(private readonly handler: (...args: TInputs) => Promise<TOutput>) {}
-  
-  async fire(key: string, ...args: TInputs): Promise<TOutput> {
-    /**
-     * insert your code here
-     */
-  
-    return;
-  }
+    constructor(private readonly handler: (...args: TInputs) => Promise<TOutput>) { }
+    private cache = new Map<string, TOutput>()
+
+    async fire(key: string, ...args: TInputs): Promise<TOutput> {
+        const release = await mutex.acquire();
+        try {
+            let cacheKey = JSON.stringify(key + args as string);
+            let keyExists = this.cache.has(cacheKey);
+
+            if (keyExists) {
+                return this.cache.get(cacheKey)!;
+            }
+
+            let result = await this.handler(...args);
+            this.cache.set(cacheKey, result);
+            return result;
+
+        } finally {
+            release();
+        }
+    }
 }
